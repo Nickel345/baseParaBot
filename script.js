a62eda8bc4910b85ccba2d7d2b4a45ca7313e394
NicoSF <salazarnico20@gmail.com>
2026-04-13 01:36:17 +0000

    Pretty much everything

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Crear las listas de imágenes, títulos y frases
 const imagenes = [
+  "https://guiltygear-wiki-gg.translate.goog/images/thumb/SotG_single_cover.jpg/600px-SotG_single_cover.jpg?33ae04&_x_tr_sl=en&_x_tr_tl=es&_x_tr_hl=es&_x_tr_pto=tc",
   "https://www.arcsystemworks.com/wp-content/uploads/2023/01/GGSTORIGINAL-SOUNDTRACK-Necessary-Discrepancy_image.jpg",
   "https://www.arcsystemworks.com/wp-content/uploads/2023/01/GGSTORIGINAL-SOUNDTRACK-Necessary-Discrepancy_image.jpg",
   "https://www.arcsystemworks.com/wp-content/uploads/2023/01/GGSTORIGINAL-SOUNDTRACK-Necessary-Discrepancy_image.jpg",
@@ -24,8 +25,21 @@ const imagenes = [
   "https://www.arcsystemworks.com/wp-content/uploads/2023/01/GGSTORIGINAL-SOUNDTRACK-Necessary-Discrepancy_image.jpg",
   "https://www.arcsystemworks.com/wp-content/uploads/2023/01/GGSTORIGINAL-SOUNDTRACK-Necessary-Discrepancy_image.jpg",
   "https://www.arcsystemworks.com/wp-content/uploads/2023/01/GGSTORIGINAL-SOUNDTRACK-Necessary-Discrepancy_image.jpg",
-  "https://www.arcsystemworks.com/wp-content/uploads/2023/01/GGSTORIGINAL-SOUNDTRACK-Necessary-Discrepancy_image.jpg",
-  
+  "https://guiltygear.wiki.gg/images/thumb/Ggst_the_town_inside_me_cover.png/600px-Ggst_the_town_inside_me_cover.png?a520b4",
+  "https://guiltygear.wiki.gg/images/thumb/Ggst_the_hourglass_cover.png/600px-Ggst_the_hourglass_cover.png?986eb2",
+  "https://guiltygear.wiki.gg/images/thumb/Ggst_the_circle_cover.png/600px-Ggst_the_circle_cover.png?f75d1f",
+  "https://guiltygear.wiki.gg/images/thumb/Ggst_the_gravity_cover.png/600px-Ggst_the_gravity_cover.png?5f0bbf",
+  "https://guiltygear.wiki.gg/images/thumb/Ggst_just_lean_cover.png/600px-Ggst_just_lean_cover.png?ca5448",
+  "https://guiltygear.wiki.gg/images/thumb/ExtrasCover.jpg/600px-ExtrasCover.jpg?4eb04d",
+  "https://guiltygear.wiki.gg/images/thumb/SymphonyCover.jpg/600px-SymphonyCover.jpg?686836",
+  "https://guiltygear.wiki.gg/images/thumb/UpsandDownsCover.jpg/600px-UpsandDownsCover.jpg?f2a704",
+  "https://guiltygear.wiki.gg/images/thumb/GGST_Radiant_Dawn_Cover.png/600px-GGST_Radiant_Dawn_Cover.png?b271bf",
+  "https://guiltygear.wiki.gg/images/thumb/A_Tenth_of_Myself_Cover_Image.webp/600px-A_Tenth_of_Myself_Cover_Image.webp?3e8b2c",
+  "https://guiltygear.wiki.gg/images/thumb/GGST_CARPE_DIEM_Cover.png/600px-GGST_CARPE_DIEM_Cover.png?eb5f5c",
+  "https://guiltygear.wiki.gg/images/thumb/THIHSITHTTH.jpg/599px-THIHSITHTTH.jpg?3f8623",
+  "https://guiltygear.wiki.gg/images/thumb/Masterofyourownsoul.jpg/600px-Masterofyourownsoul.jpg?cbe1f5",
+  "https://guiltygear.wiki.gg/images/thumb/TRICK_OR_CHAOS_Cover.jpg/600px-TRICK_OR_CHAOS_Cover.jpg?df0282",
+  "https://guiltygear.wiki.gg/images/thumb/GGXrdR_ost_cover.jpg/600px-GGXrdR_ost_cover.jpg?97ea4f",
 ];
 
 const titulos = [
@@ -53,7 +67,22 @@ const titulos = [
   "Hellfire",
   "漉祭 -Rock Parade-",
   "Mirror of the World",
-  "Talk About You"
+  "Talk About You",
+  "The Town Inside Me",
+  "The Hourglass",
+  "The Circle",
+  "The Gravity",
+  "Just Lean",
+  "Extras",
+  "Symphony",
+  "Ups and downs",
+  "Radiant Dawn",
+  "A Tenth of Myself",
+  "CARPE DIEM",
+  "Tryinh Hard is Hard, so I Try Hard to To Try Hard",
+  "Master of Your Own Soul",
+  "Shelter from the Rain",
+  "Sky Should be High",
 ];
 
 const frases = [
@@ -67,7 +96,7 @@ const frases = [
   "Chipp Zanuff",
   "May",
   "Axl Low",
-  "Other",
+  "Arcade Credits",
   "Zato-1",
   "Ramlethal Valentine",
   "Arena Mode Boss",
@@ -81,7 +110,22 @@ const frases = [
   "Leo Whitefang",
   "Anji",
   "Baiken",
-  "Story Mode Credits"
+  "Story Mode Credits",
+  "Bridget",
+  "Sin Kiske",
+  "Bedman?",
+  "Asuka R. Kreutz",
+  "Jhonny",
+  "Elphelt Valentine",
+  "A.B.A.",
+  "Slayer",
+  "Dizzy",
+  "Venom",
+  "Unika",
+  "Jam Kuradoberi",
+  "2.0 Update Theme",
+  "Alternative Arcade mode credits",
+  "Pre-fight theme"
 ];
 
 // Trae los elementos del HTML que tienen ese id
